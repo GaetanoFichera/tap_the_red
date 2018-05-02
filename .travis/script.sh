a478a5c7d028b5450ebf2b3b645670106c86e52f
gaetano <gaetano.fichera93@gmail.com>
2018-05-02 19:30:27 +0200

    trying travis rvm update into script.sh too

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+    rvm get stable
+fi
 ./flutter/bin/flutter -v build ios --no-codesign
