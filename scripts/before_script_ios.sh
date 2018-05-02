90b1dad71c29c3083293e76ccdaa2965b6b182c8
gaetano <gaetano.fichera93@gmail.com>
2018-05-02 18:15:28 +0200

    travis copiato da repo flutter

--- scripts/before_script_ios.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-pip install six
-brew update
-brew install --HEAD libimobiledevice
-brew install ideviceinstaller
-brew install ios-deploy
-git clone https://github.com/flutter/flutter.git -b alpha --depth 1
--- scripts/script_ios.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-./flutter/bin/flutter -v build ios --no-codesign
