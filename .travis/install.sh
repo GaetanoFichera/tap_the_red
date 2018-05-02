e58535be969ba9c84814e24abf6224fffaf01a1d
gaetano <gaetano.fichera93@gmail.com>
2018-05-02 19:19:31 +0200

    trying travis rvm update

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+    rvm get stable
+fi
 pip2 install six
 brew update
 brew install --HEAD libimobiledevice
