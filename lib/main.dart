f775b35690541b9490629eaff8546577caf4ab6d
gaetano <gaetano.fichera93@gmail.com>
2018-05-02 17:43:13 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tap_the_red/View/HomePage.dart';
 
 void main() {
+  //controllare se si può fare a meno di questa funzione
   //SharedPreferences.setMockInitialValues({});
   runApp(new HomePageSL());
 }
