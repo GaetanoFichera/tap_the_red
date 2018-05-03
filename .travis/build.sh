0bbf86d76a392cc2cc12e205b396b73b7ed6373d
GaetanoFichera <gaetano.fichera93@gmail.com>
2018-05-03 20:04:59 +0200

    migrated flutter git to stable; deploy only .ipa

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,5 +2,8 @@
 
 sudo motion update || echo 'up to date'
 ./flutter/bin/flutter -v build ios --no-codesign
-zip -r build_ios.zip build/ios
-git add build_ios.zip
+mkdir Payload
+cp -R build/ios/Release-iphoneos/Runner.app Payload
+zip -r Payload.zip Payload
+mv Payload.zip app.ipa
+git add app.ipa
